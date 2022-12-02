5f0b06d902ae49224cc52a9d763daecd536fcc28
Eric Onyango <eric.onyango@student.moringaschool.com>
2022-12-02 10:18:42 +0300

    Completed Assignment, Finally.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(people,surprise) {
+    let thankPeople = []
+    for (let i = 0; i < people.length; i++) {
+      thankPeople.push(`Thank you, ${people[i]}, for the wonderful ${surprise} gift!`)
+    }
+    return thankPeople
+  }
+  
+  function countDown(num0) {
+    while (num0 > 0) {
+      console.log(num0);
+      num0 -= 1;
+    }
+    console.log(num0);
+  }
